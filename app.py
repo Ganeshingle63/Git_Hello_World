fe238fe7a0093069ee2677cb1bfa274408d44bdb
Ganeshingle63 <123880864+Ganeshingle63@users.noreply.github.com>
2023-02-14 19:48:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route('/')
 def Hello_World():
     print('Hello World From GitHub')
-
+    first='Ganesh'
+    last='Ingle'
+    return render_template('hello.html',first_name=first,last_name=last)
 
 def run():
     Hello_World()
